e2f7b84ebdbb8d89a83f54bf8d4ebf15f81b7787
Christopher Tastad <ctastad@gmail.com>
2021-08-20 14:41:40 -0500

    unified templates for base cellranger case and sample params

--- tmp.sh
+++ tmp.sh
@@ -1,27 +1,48 @@
-#DIR=$1
-#FILE=$2
-#cd $DIR
-COUNT=8 		# number of samples
 
-module load cellranger parallel
-cd /hpc/users/tastac01/ctastad/projects/conv_covid_seq/analysis
+#!/bin/bash
+#BSUB -J cr_count
+#BSUB -P acc_untreatedIBD
+#BSUB -W 02:00
+#BSUB -q express
+#BSUB -n 2
+#BSUB -R rusage[mem=64GB]
+#BSUB -R span[hosts=1]
+#BSUB -o ../logs/output_%J.stdout
+#BSUB -eo ../logs/error_%J.stderr
+#BSUB -L /bin/bash
 
-## direct path of config cs should be give as first argument
-cr_multi () {
-  cellranger multi \
-    --id=cr_multi_$1_`date '+%F_%H%M'` \
-    --csv=$1.csv
-}
+# this script should be executed from site of output dir
+# fastqs should be arranged in dirs arranged by sample
+# dirs should carry sample name
 
-export -f cr_multi 		# export cr function to env for parallel
+# params
+PIPELINE=count               # name of cr pipeline
+PROJ_DIR=/home               # /path/to/proj/dir
+REF_DIR=/ref_dir                # /path/to/gene/refs
+GENE_REF=/gene_ref               # name of gene ref
+SAMPLE_DIR=$PROJ_DIR/fastqs   # /proj/subdir/to/fastqs
 
-# iterate through generation of config csv per sample
-for i in $(seq 1 $COUNT); do
-  cp cellranger_multi_config_template.csv TD005310-CP-$i.csv
-  sed -i -e "s/\*\*\*/$i/g" TD005310-CP-$i.csv
+################################################################################
+
+#module load cellranger parallel
+
+if [[ $PIPELINE == "count" ]]
+then
+    GENOME_LABEL=transcriptome
+else
+    GENOME_LABEL=reference
+fi
+
+# core cellranger function
+for i in *
+#for i in $(seq 1 $COUNT)
+do
+echo "
+cellranger $PIPELINE \
+  --id=cr_$PIPELINE_$i_`date '+%F_%H%M'` \
+  --$GENOME_LABEL=$REF_DIR/$GENE_REF \
+  --fastqs=$SAMPLE_DIR \
+  #--sample=$SAMPLE_PREFIX
+  --sample=$i
+"
 done
-
-# execute cellranger in parallel
-find TD005310-CP-* | cut -d. -f1 | parallel cr_multi {}
-
-rm TD005310-CP-*
